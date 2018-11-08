a624a612f1bbff0fad836b1324fe320406c8b1b6
Tom Burger <tvburger@Thomass-MacBook-Air.local>
2018-11-08 23:52:05 +0100

    Clean up of ShortExample

--- example/deploy/src/main/java/net/tvburger/up/example/deploy/ShortExample.java
+++ example/deploy/src/main/java/net/tvburger/up/example/deploy/ShortExample.java
@@ -14,8 +14,6 @@ public final class ShortExample {
     public static void main(String[] args) throws Exception {
         Environment environment = LocalUpRuntimeFactory.createEnvironment(Jetty9Implementation.get());
         try {
-            Thread.sleep(10_000);
-
             EnvironmentManager manager = environment.getManager();
             manager.deploy(ServiceDefinition.Factory.create(DependencyService.class, DependencyServiceImpl.class));
             manager.deploy(ServiceDefinition.Factory.create(ExampleService.class, ExampleServiceImpl.class, "Howdy,", DependencyService.class));
@@ -23,21 +21,7 @@ public final class ShortExample {
                     .withEndpointTechnology(Jsr340.Specification.get())
                     .withEndpointDefinition(ExampleServlet.class, ExampleService.class)
                     .withSetting("mapping", "/abc/*").build());
-
-            System.out.println(environment.getService(ExampleService.class).getInterface().sayHelloTo("Tom"));
             Example.printEnvironment(environment);
-
-            Thread.sleep(10_000);
-
-            manager.deploy(new EndpointDefinition.Builder()
-                    .withEndpointTechnology(Jsr340.Specification.get())
-                    .withEndpointDefinition(ExampleServlet.class, ExampleService.class)
-                    .withSetting("mapping", "/def/*").build());
-
-            Thread.sleep(60_000);
-
-            environment.getRuntime().getManager().stop();
-            environment.getRuntime().getManager().destroy();
         } finally {
             LocalUpRuntimeFactory.destroyEnvironment(environment);
         }
